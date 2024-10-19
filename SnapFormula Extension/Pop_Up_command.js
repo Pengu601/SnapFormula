47d0f84937bcf61e9dc19c60a13d84fa919b6259
Pengu601 <111535906+Pengu601@users.noreply.github.com>
2024-10-18 23:08:39 -0400

    debugging

--- SnapFormula Extension/Pop_Up_command.js
+++ SnapFormula Extension/Pop_Up_command.js
@@ -1,4 +1,5 @@
 document.getElementById('activateSnippingTool').addEventListener('click', () => {
+  console.log('Button clicked');
   chrome.tabs.query({active: true, currentWindow: true}, (tabs) => {
     chrome.scripting.executeScript({
       target: {tabId: tabs[0].id},
