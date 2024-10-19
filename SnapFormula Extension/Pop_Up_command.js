aa6174c0ca0a1b740aa860c77fa807f6f6da0ca5
Aiden Coffey <148676885+AidenCoffey@users.noreply.github.com>
2024-10-18 22:40:59 -0400

    Update Pop_Up_command.js

--- SnapFormula Extension/Pop_Up_command.js
+++ SnapFormula Extension/Pop_Up_command.js
@@ -2,7 +2,7 @@ document.getElementById('activateSnippingTool').addEventListener('click', () => 
   chrome.tabs.query({active: true, currentWindow: true}, (tabs) => {
     chrome.scripting.executeScript({
       target: {tabId: tabs[0].id},
-      files: ['snippingTool.js']
+      files: ['Snipping_Function.js']
     });
   });
 });
